373a079868c0845bc0e4a00f2013c237d80c082c
cieza <elise.cieza@gmail.com>
2015-05-13 09:42:45 -0300

    3 atacantes, taxa 140/s. Experimentos 89 e 90.

--- scenarios/ndn-with-cache-with-mob-v2v-pollution.cc
+++ scenarios/ndn-with-cache-with-mob-v2v-pollution.cc
@@ -306,7 +306,7 @@ main (int argc, char *argv[])
     
     ndn::AppHelper consumerHelperAttack ("ns3::ndn::ConsumerCbr");
     consumerHelperAttack.SetPrefix (prefixAttack);
-    consumerHelperAttack.SetAttribute ("Frequency", DoubleValue (120.0));
+    consumerHelperAttack.SetAttribute ("Frequency", DoubleValue (140.0));
     
     /*consumerHelperAttack.Install (nodes.Get (5));
      consumerHelperAttack.Install (nodes.Get (10));
