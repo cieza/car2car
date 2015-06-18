7056aa23a99b53e52651f40619a81e56e53d7785
cieza <elise.cieza@gmail.com>
2015-06-18 01:58:45 -0300

    Reproduzir experimento 18, rodada 11, erro no arquivo. 5 atacantes, taxa 40/s. Cenário com mobilidade e poluidor.

--- scenarios/ndn-with-cache-with-mob-v2v-pollution.cc
+++ scenarios/ndn-with-cache-with-mob-v2v-pollution.cc
@@ -306,7 +306,7 @@ main (int argc, char *argv[])
     
     ndn::AppHelper consumerHelperAttack ("ns3::ndn::ConsumerCbr");
     consumerHelperAttack.SetPrefix (prefixAttack);
-    consumerHelperAttack.SetAttribute ("Frequency", DoubleValue (140.0));
+    consumerHelperAttack.SetAttribute ("Frequency", DoubleValue (40.0));
     
     /*consumerHelperAttack.Install (nodes.Get (5));
      consumerHelperAttack.Install (nodes.Get (10));
@@ -354,33 +354,33 @@ main (int argc, char *argv[])
         }
     }
     
+    encontrou = false;
+    while(!encontrou)
+    {
+        num_rand = rand() % numberOfCars;
+        if (nodes_map.find(num_rand) == nodes_map.end())
+        {
+            nodes_map[num_rand] = 1;
+            encontrou = true;
+            consumerHelperAttack.Install (nodes.Get (num_rand));
+            cout<<"Atacante: "<<num_rand<<"\n";
+        }
+    }
+    
+    encontrou = false;
+    while(!encontrou)
+    {
+        num_rand = rand() % numberOfCars;
+        if (nodes_map.find(num_rand) == nodes_map.end())
+        {
+            nodes_map[num_rand] = 1;
+            encontrou = true;
+            consumerHelperAttack.Install (nodes.Get (num_rand));
+            cout<<"Atacante: "<<num_rand<<"\n";
+        }
+    }
+    
     /*encontrou = false;
-    while(!encontrou)
-    {
-        num_rand = rand() % numberOfCars;
-        if (nodes_map.find(num_rand) == nodes_map.end())
-        {
-            nodes_map[num_rand] = 1;
-            encontrou = true;
-            consumerHelperAttack.Install (nodes.Get (num_rand));
-            cout<<"Atacante: "<<num_rand<<"\n";
-        }
-    }
-    
-    encontrou = false;
-    while(!encontrou)
-    {
-        num_rand = rand() % numberOfCars;
-        if (nodes_map.find(num_rand) == nodes_map.end())
-        {
-            nodes_map[num_rand] = 1;
-            encontrou = true;
-            consumerHelperAttack.Install (nodes.Get (num_rand));
-            cout<<"Atacante: "<<num_rand<<"\n";
-        }
-    }
-    
-    encontrou = false;
     while(!encontrou)
     {
         num_rand = rand() % numberOfCars;
